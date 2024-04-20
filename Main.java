c650aa9971e417dc406d14f2cf2facd452109def
Tiv-Barlow <164186574+Tiv-Barlow@users.noreply.github.com>
2024-04-20 07:20:09 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+//M04 Programming Assignment (3)
+//Ivy Tech Community College
+//SDEV 200 - Java
+//Professor Bumgardner
+//Nativida Muhammad
+//14 April 2024
+
+public class Main {
+    public static void main(String[] args) {
+        // Creates a new instance of TwoWayLinkedList
+        TwoWayLinkedList<String> list = new TwoWayLinkedList<>();
+
+        // Adds elements to the list
+        list.add("Apple");
+        // add other operators as needed here.
+    }
+}
+
